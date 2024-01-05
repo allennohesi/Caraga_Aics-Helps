d35c7130d212effc499fe0a6f27f311ba8cd6bff
Phillipe Monares <phillipemonares@gmail.com>
2024-01-05 16:38:48 +0800

    searching client and bene

--- api/requests/serializers.py
+++ api/requests/serializers.py
@@ -6,8 +6,8 @@ from app.finance.models import finance_voucher, finance_voucherData
 
 class TransactionSerializer(serializers.ModelSerializer):
     tracking_number = serializers.CharField(source='transaction.tracking_number')
-    client = serializers.CharField(source='transaction.client.get_client_fullname', read_only=True, default=None)
-    beneficiary = serializers.CharField(source='transaction.bene.get_client_fullname', read_only=True, default=None)
+    client = serializers.CharField(source='transaction.client.client_bene_fullname', read_only=True, default=None)
+    beneficiary = serializers.CharField(source='transaction.bene.client_bene_fullname', read_only=True, default=None)
     verified_time_start = serializers.DateTimeField(format="%b %d, %Y - %H:%M %p", read_only=True)
     swo = serializers.CharField(source='transaction.swo.get_fullname', read_only=True, default=None)
     #swo_lastname = serializers.CharField(source='transaction.swo.last_name', read_only=True, default=None)
