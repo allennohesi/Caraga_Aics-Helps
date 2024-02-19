9e6699256a92cffcadc84b4a7b3acd5e49e8805d
Phillipe Monares <phillipemonares@gmail.com>
2024-02-19 12:15:46 +0800

    remove the pagination on background task

--- api/requests/views.py
+++ api/requests/views.py
@@ -21,7 +21,6 @@ class LargeResultsSetPagination(PageNumberPagination):
 class TransactionPerSession(generics.ListAPIView):
     serializer_class = TransactionSerializer
     permission_classes = [IsAuthenticated]
-    pagination_class = LargeResultsSetPagination
     
     def get_queryset(self):
         if self.request.query_params.get('user'):
