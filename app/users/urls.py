89fafc0c882f8b2752d423407689773780cbd68a
Allen Efren Hilado <aeohilado@dswd.gov.ph>
2024-05-21 15:05:13 +0800

    Polishing the system

--- app/users/urls.py
+++ app/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from app.users.views import user_list, edit_user, get_role, change_password, user_profile, feedback, error_logs, clientupdatehistory
+from app.users.views import user_list, edit_user, get_role, change_password, user_profile, feedback, error_logs, clientupdatehistory, clienthistorymodal
 
 urlpatterns = [
     path('list/', user_list, name='user_list'),
@@ -10,5 +10,6 @@ urlpatterns = [
     path('user_profile/', user_profile, name='user_profile'),
     path('feedback/', feedback, name='feedback'),
     path('error_logs/', error_logs, name='error_logs'),
-    path('clientupdatehistory/', clientupdatehistory, name='clientupdatehistory')
+    path('clientupdatehistory/', clientupdatehistory, name='clientupdatehistory'),
+    path('clienthistorymodal/modal/<int:pk>', clienthistorymodal, name='clienthistorymodal')
 ]
