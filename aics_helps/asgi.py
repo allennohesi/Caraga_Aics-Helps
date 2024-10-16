4044929991e38ef24b737599e315ee4d59845ac3
Allen Efren Hilado <aeohilado@dswd.gov.ph>
2024-10-16 10:48:51 +0800

    changes in asgi py

--- aics_helps/asgi.py
+++ aics_helps/asgi.py
@@ -1,16 +1,17 @@
-"""
-ASGI config for aics_helps project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+#from your_app import routing  # Replace with your actual routing module
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aics_helps.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    # (http->django views is added by default)
+    # "websocket": AuthMiddlewareStack(
+    #     URLRouter(
+    #         routing.websocket_urlpatterns  # Update with your websocket URL patterns
+    #     )
+    # ),
+})
