c46cc9947d6809589b683db346f9c36a86589d84
Allen Efren Hilado <aeohilado@dswd.gov.ph>
2024-10-23 21:06:28 +0800

    put the initation behind

--- aics_helps/asgi.py
+++ aics_helps/asgi.py
@@ -2,12 +2,15 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from app.chat import routing  # Replace with your actual routing module
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aics_helps.settings')
 
+application = get_asgi_application()
+
+from app.chat import routing  # Replace with your actual routing module
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": application,
     # (http->django views is added by default)
     "websocket": AuthMiddlewareStack(
         URLRouter(
